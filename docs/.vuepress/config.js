b5530d977ac49c79947f3fef0712f031e209355d
vittominacori <vittoriominacori@gmail.com>
2019-01-14 22:35:54 +0100

    add docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'Solidity Starter Kit',
+  description: 'A starter kit for Ethereum Smart Contracts development',
+  base: '/solidity-starter-kit/',
+  ga: 'UA-115756440-2',
+  themeConfig: {
+    repo: 'vittominacori/solidity-starter-kit',
+    sidebar: 'auto',
+  },
+};
