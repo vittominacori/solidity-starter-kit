9ec0b37801f43484db69eaff54c04a5c611df00a
vittominacori <vittoriominacori@gmail.com>
2019-12-31 12:27:31 +0100

    update client to use istanbul

--- .solcover.js
+++ .solcover.js
@@ -2,11 +2,15 @@ module.exports = {
     norpc: true,
     testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
     compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
+    client: require('ganache-cli'),
+    providerOptions: {
+      hardfork: 'istanbul',
+    },
     copyPackages: [
-        'eth-token-recover',
-        '@openzeppelin/contracts',
+      'eth-token-recover',
+      '@openzeppelin/contracts',
     ],
     skipFiles: [
-        'mocks'
+      'mocks',
     ],
 };
